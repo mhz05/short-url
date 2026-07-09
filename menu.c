b109a78d75a5a60f33ae7459046d34aa81ab4c2a
mhz <987215490@qq.com>
2026-07-09 16:14:47 +0800

    第三次提交 增添menu菜单文件

---
+++ menu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "menu.h"
+
+void menu(void)
+{
+    printf("\n");
+    printf("=====================================\n");
+    printf("         Tiny URL 短地址服务\n");
+    printf("=====================================\n");
+    printf("1. 生成短地址\n");
+    printf("2. 解析短地址\n");
+    printf("3. 查看所有记录\n");
+    printf("4. 查看访问统计\n");
+    printf("5. 设置有效访问次数\n");
+    printf("6. 退出程序\n");
+    printf("=====================================\n");
+    printf("请输入操作：");
+}
---
+++ menu.h
@@ -0,0 +1,7 @@
+#ifndef __MENU_H__
+#define __MENU_H__
+
+// 显示菜单
+void menu(void);
+
+#endif
